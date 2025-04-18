0e87cbdd5fa30dc1b378b0da7040c9f093e5e7ed
Sekt4nt <dtoronchenko@gmail.com>
2025-04-18 13:04:51 +0100

    Added premake

---
+++ premake5.lua
@@ -0,0 +1,49 @@
+workspace "DllInjector"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "DllInjector"
+    location "DllInjector"
+	kind "WindowedApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+        "%{prj.name}/**.aps",
+        "%{prj.name}/**.rc",
+	}
+
+    includedirs
+    {
+        "%{prj.name}"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"	
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "On"
